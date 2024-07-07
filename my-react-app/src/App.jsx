09b686dcf31601d4cb379760f076049a013b218d
Jianyi (Birdy) Lu <153916948+BirdyLu@users.noreply.github.com>
2024-07-07 20:42:34 +1000

    a progress everyday keeps an apple away

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button/Button.jsx'
 
 function App() {
 
@@ -9,7 +10,9 @@ function App() {
     <>
       <Header/>
       <Food/>
-      <Card/>
+      <Button/><button>default button</button>
+      <br/>
+      <Card/><Card/><Card/><Card/><Card/>
       <Footer/>
     </>
   );
--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,11 +1,14 @@
 function Card(){
     return (
         <div className="card">
-            <img></img>
-            <h2>Cardarrrrr</h2>
-            <p>
-                What? Why u asking me? Dont tell me what to do
-            </p>
+            <div className="card-body">
+                <img className="card-img-top" alt="stop looking here" src="https://www.w3schools.com/images/lynx_landing.webp"></img>
+                <h2 className="card-title">Cardarrrrr</h2>
+                <p className="card-text">
+                    What? Why u asking me? Dont tell me what to do
+                </p>
+                <a href="#" className="btn btn-primary stretched-link">See Profile</a>
+            </div>
         </div>
     );
 }
