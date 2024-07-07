5552bfa2c80554532c8e16c18f4b70e76a5cb5ee
Jianyi (Birdy) Lu <153916948+BirdyLu@users.noreply.github.com>
2024-07-07 19:57:46 +1000

    ?

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,13 @@
+function Card(){
+    return (
+        <div className="card">
+            <img></img>
+            <h2>Cardarrrrr</h2>
+            <p>
+                What? Why u asking me? Dont tell me what to do
+            </p>
+        </div>
+    );
+}
+
+export default Card
