3eb508503f5f501d401087a567c0487c6d5abe05
Jianyi (Birdy) Lu <153916948+BirdyLu@users.noreply.github.com>
2024-07-07 19:19:44 +1000

    uploaded directoyr

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>Contact</li></a>
+                    <a href="#"><li>Why</li></a>
+                </ul>
+            </nav>
+            <hr/>
+        </header>
+    );
+}
+
+export default Header
