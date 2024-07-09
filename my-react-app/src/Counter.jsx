37b56bc8babbb4f41534850c377ee4c4373e066b
Jianyi (Birdy) Lu <153916948+BirdyLu@users.noreply.github.com>
2024-07-10 00:51:02 +1000

    save progress

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -1,10 +1,18 @@
 import {useState} from 'react';
 
 function Counter(){
+    let name = "Jerry";
     const [name, setName] = useState(); //useState returns a variable and a setter function.
 
+    const updateName = () => {
+        setName("SpongeBob");
+    }
+
     return (
-        <div></div>
+        <div>
+            <p>Name: {name}</p>
+            <button onClick={updateName}>Set name</button>
+        </div>
     );
 }
 
